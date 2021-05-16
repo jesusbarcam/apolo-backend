f7661c34eaa67d957ba7ca326d4bbe4cc6272980
jesusbarcam <jesusbarcam@gmail.com>
2021-05-16 11:34:42 +0200

    Added createUser Route

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,12 +1,38 @@
 'use strict';
 
 //const { usersService } = require('usersService');
-const { publicUser, publicUsers, usersSchema } = require('./users.schema');
+const {
+  publicUser,
+  publicUsers,
+  usersResponseSchema,
+  createUser,
+  createUserResponseSchema,
+} = require('./users.schema');
 
 module.exports = async function (fastify, options) {
   fastify.addSchema(publicUser);
   fastify.addSchema(publicUsers);
-  fastify.get('/users', { usersSchema }, async (request, reply) => {
+  fastify.addSchema(createUser);
+
+  fastify.post(
+    '/users',
+    { createUserResponseSchema },
+    async (request, reply) => {
+      const { username, password, fullName } = request.body;
+      fastify.log.info('El usuario ha sido creado');
+      reply.code(201);
+      return await {
+        username: 'jesusBarcam',
+        fullName: 'Jesus Barajas Camacho',
+        email: 'jesusbarcam@gmail.com',
+        tlf: 657448534,
+      };
+    }
+  );
+  // TODO: Crear el registro de usuarios
+  // nuevos tanto en el flujo de fastify como
+  // añadirlos a traves de un servicio usersService
+  fastify.get('/users', { usersResponseSchema }, async (request, reply) => {
     // TODO: Obtener el listado de usuarios desde
     // nuestro servicio usersService.
     return await [
--- routes/users/users.schema.js
+++ routes/users/users.schema.js
@@ -1,8 +1,10 @@
 const USER_ID = 'publicUser';
+const CREATE_USER_ID = 'createUser';
 const USER_LIST_ID = 'publicUsers';
 
 const USER_LIST_REF = `${USER_LIST_ID}#`;
 const USER_REF = `${USER_ID}#`;
+const CREATE_USER_REF = `${CREATE_USER_ID}#`;
 
 /**
  * @schema public
@@ -29,19 +31,49 @@ const publicUsers = {
 }; // PublicUsers
 
 /**
+ * @schema public
+ * @description
+ */
+const createUser = {
+  $id: CREATE_USER_ID,
+  type: 'object',
+  properties: {
+    username: { type: 'string' },
+    password: { type: 'string' },
+    fullName: { type: 'string' },
+  },
+};
+
+/**
  * @schema response
  * @description
  */
-const usersSchema = {
-  response: {
-    200: { $ref: USER_LIST_REF },
+const usersResponseSchema = {
+  schema: {
+    response: {
+      200: { $ref: USER_LIST_REF },
+    },
   },
 }; // UsersSchema
+
+/**
+ * @schema response
+ * @description
+ */
+const createUserResponseSchema = {
+  schema: {
+    response: {
+      201: { $ref: USER_REF },
+    },
+  },
+}; // CreateUserResponseSchema
 
 module.exports = {
   USER_LIST_REF,
   USER_REF,
   publicUser,
   publicUsers,
-  usersSchema,
+  createUser,
+  usersResponseSchema,
+  createUserResponseSchema,
 };
